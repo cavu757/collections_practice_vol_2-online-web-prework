58a4a6e4793a4c92e999cf4e188bbd82638c4239
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-02-10 14:24:45 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -99,24 +99,16 @@ def organize_schools(schools)
   hsh2 = {}
   hsh3 = {}
   a_ct=0 
-  
-  
-  
   hsh1 = schools.group_by{|k, v| v}
-  
   while a_ct < hsh1.count 
     arr1 = hsh1.values[a_ct].flatten
     hsh1.keys[a_ct]
     arr1.delete(hsh1.keys[a_ct])
-    
     arr2 = hsh1.keys[a_ct].flatten
     arr2.delete(:location)
-     
     hsh2 = {arr2[0] => arr1}
     hsh3 = hsh3.merge(hsh2)
-  
     a_ct += 1 
   end
   hsh3
-
 end
